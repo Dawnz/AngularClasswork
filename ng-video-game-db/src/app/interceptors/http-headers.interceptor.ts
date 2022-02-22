f04dd7b446bae9562389ecfac5e76ae4ce14bdfd
Dawnz <dawnz1@hotmail.com>
2022-02-22 17:51:52 -0500

    Completed Demo of video Game

---
+++ ng-video-game-db/src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        key: '4f9fc996dad740c4ad9e51eae37e8df3',
+      },
+    });
+    // console.log(req.headers);
+
+    return next.handle(req);
+  }
+}
