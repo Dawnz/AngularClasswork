01fe94ea6e76f897e5a01f5b2b40d88c9b09e400
Dawnz <dawnz1@hotmail.com>
2022-02-22 18:08:15 -0500

    added routing example

---
+++ routing/src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+const routes: Routes = [
+  {
+    path: 'first-component/:id',
+    component: FirstComponent,
+    children: [
+      {
+        path: 'child-a',
+        component: ChildAComponent,
+      },
+      {
+        path: 'child-b',
+        component: ChildBComponent,
+      },
+    ],
+  },
+  {
+    path: 'second-component',
+    component: SecondComponent,
+  },
+  {
+    path: '**',
+    component: SecondComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
