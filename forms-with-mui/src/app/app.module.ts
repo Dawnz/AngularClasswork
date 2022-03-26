a9a768881b7564fa66da1c2b62e94bda04065917
Dawnz <dawnz1@hotmail.com>
2022-03-26 14:58:20 -0500

    added materials module to reduce overhead in app

--- forms-with-mui/src/app/app.module.ts
+++ forms-with-mui/src/app/app.module.ts
@@ -3,32 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { GoogleFormComponent } from './components/google-form/google-form.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { ReactiveFormsModule } from '@angular/forms';
 import { ConfirmEqualValidatorDirective } from './directives/confirm-equal-validator.directive';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialsModule } from './materials/materials.module';
+
+//passes along metadata info for the module
 @NgModule({
+  //accepts an array of declarables
+  // declarables includes Components, Pipes and Directives
   declarations: [
     AppComponent,
     GoogleFormComponent,
     ConfirmEqualValidatorDirective,
   ],
+  //imports accepts an array of modules to be loaded when this module starts
+  //imports by default are private to the module called
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCheckboxModule,
-    MatButtonModule,
-    MatIconModule,
     ReactiveFormsModule,
+    MaterialsModule,
   ],
+  //providers accepts an array of services or guards to be accessed by declarables of this module
   providers: [],
+  //Angular creates the EntryComponent(s) that is called by bootstrap.
+  //This is usually AppComponent
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ forms-with-mui/src/app/materials/materials.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+const material = [
+  MatInputModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatCheckboxModule,
+  MatIconModule,
+];
+// Common Module Exports all the basic Angular directives and pipes, such as NgIf, NgForOf, DecimalPipe, and so on.
+@NgModule({
+  declarations: [],
+  // imports: [CommonModule, material],
+
+  //exports makes the listed elements available to other modules.
+  //exports changes the private access of modules to public
+  exports: [material],
+})
+export class MaterialsModule {}
